c64b4417f51c95fa2481c32f7051feb53844c9c9
mdporter <mportertime@gmail.com>
2019-09-25 08:53:31 -0400

    added birth month hypothesis material

---
+++ R/birthmonth-lab.R
@@ -0,0 +1,39 @@
+###----------------------------------------------------------------------------
+## birthmonth.R 
+## In-class exercises
+## Testing the Birth Month Hypothesis 
+##   Does the month of your birth impact your chances of success? 
+###----------------------------------------------------------------------------
+
+#-- Load Required Libraries
+library(tidyverse)
+library(Lahman)
+
+#----------------------------------
+#-- Birth Months and Baseball
+#----------------------------------
+
+# 1. Install the `Lahman` R package
+
+# 2. Load the `Lahman` package and the `Master` data
+
+# 3. Take a peek (or `glimpse`) at the `Master` data. Does it contain what we need to test the birth month hypothesis? 
+
+
+#----------------------------------
+#-- Visualizing the Data
+#----------------------------------
+
+# 1. What calculations do we need to perform? 
+  
+# 2. What type of plot should we make? 
+
+  
+#----------------------------------
+#-- Testing the hypothesis
+#----------------------------------
+
+# 1. How should we test the hypothesis? 
+  
+# 2. Any other considerations before we make a conclusion? 
+
