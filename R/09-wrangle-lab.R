2d976792322bb33eab9abb511beddd33a189656d
mdporter <mportertime@gmail.com>
2019-09-26 20:27:37 -0400

    added challenge materials

---
+++ R/09-wrangle-lab.R
@@ -0,0 +1,23 @@
+## 09-wrangle-lab.R
+
+## A group of students have taken exams in math, science, and English. 
+## (a) You want to combine these scores in order to determine a single 
+##     performance indicator for each student. 
+## (b) Additionally, you want to assign an A to the top 20\% of students, 
+##     a B to the next 20\%, and so on. 
+## (c) Finally, you want to sort the students alphabetically by last name.
+## (d) In addition, you need to create a Cleveland Dot Plot to show the student 
+##     name, their score, and their letter grade.
+
+
+student = c("John Davis","Angela Williams","Bullwinkle Moose",
+             "David Jones","Janice Markhammer",
+             "Cheryl Cushing","Reuven Ytzrhak",
+             "Greg Knox","Joel England","Mary Rayburn")
+math = c(502, 600, 412, 358, 495, 512, 410, 625, 573, 522)
+science = c(95, 99, 80, 82, 75, 85, 80, 95, 89, 86)
+english = c(25, 22, 18, 15, 20, 28, 15, 30, 27, 18)
+
+
+
+
